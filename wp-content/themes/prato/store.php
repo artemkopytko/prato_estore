1963a1ff6d630574415b47fa72f6d67cb0a4342e
artemkopytko <kopytkoartem@gmail.com>
2018-04-24 16:56:50 +0300

    Fetching product style for desc

--- wp-content/themes/prato/store.php
+++ wp-content/themes/prato/store.php
@@ -9,32 +9,32 @@
             <div class="link-parent"><a href="/prato/">Главная</a> / Каталог Товара</div>
             <div class="store-products-filter">
                 <a class="ml-0 filter-main">Каталог Товара</a>
-	            <?php
+				<?php
 
-	            $number = null;
-	            $orderby = '';
-	            $hide_empty = 0;
-	            $ids = array();
+				$number = null;
+				$orderby = '';
+				$hide_empty = 0;
+				$ids = array();
 
-	            $args = array(
-		            'taxonomy'   => "product_cat",
-		            'number'     => $number,
-		            'orderby'    => $orderby,
-		            'order'      => $order,
-		            'hide_empty' => $hide_empty,
-		            'include'    => $ids
-	            );
-	            $product_categories = get_terms($args);
+				$args = array(
+					'taxonomy'   => "product_cat",
+					'number'     => $number,
+					'orderby'    => $orderby,
+					'order'      => $order,
+					'hide_empty' => $hide_empty,
+					'include'    => $ids
+				);
+				$product_categories = get_terms($args);
 
-	            foreach( $product_categories as $cat )
-	            {
-		            if($cat->name === 'Uncategorized')
-			            continue;
-		            $link = get_category_link($cat->term_id);
-		            echo
-		                 '<a href="' . $link . '">' . $cat->name . '</a>';
-	            }
-	            ?>
+				foreach( $product_categories as $cat )
+				{
+					if($cat->name === 'Uncategorized')
+						continue;
+					$link = get_category_link($cat->term_id);
+					echo
+						'<a href="' . $link . '">' . $cat->name . '</a>';
+				}
+				?>
 
             </div>
             <div class="store-products">
@@ -42,30 +42,40 @@
 				<?php
 				global $paged;
 
+
 				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 				$args = array(
 					'post_type'=>'product',
-					'posts_per_page' => 1,
+					'posts_per_page' => 21,
 					'paged' => $paged,
 				);
 
 				$loop = new WP_Query( $args );
 				$i = 0;
 
+
 				if ( $loop->have_posts() ) {
+					$terms = get_terms( 'product_tag' );
+					$term_array = array();
+					if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+						foreach ( $terms as $term ) {
+							$term_array[] = $term->name;
+						}
+					}
 					while ( $loop->have_posts() ) : $loop->the_post();
 
 						global $product;
 
-                        if ( has_post_thumbnail( $product->get_id() ) ) {
-			                $attachment_ids[0] = get_post_thumbnail_id( $product->get_id() );
-			                $attachment        = wp_get_attachment_image_src( $attachment_ids[0], 'full' );
-		                }
+
+						if ( has_post_thumbnail( $product->get_id() ) ) {
+							$attachment_ids[0] = get_post_thumbnail_id( $product->get_id() );
+							$attachment        = wp_get_attachment_image_src( $attachment_ids[0], 'full' );
+						}
 
 //					$product->get_attribute( 'your_attr' );
 
-		                echo '
+						echo '
                         <div class="product">
                         <div class="product-image"
                         style="background-image: url(' . $attachment[0] . ';)" data-id="' . $product->get_id() . '"></div>
@@ -76,7 +86,7 @@
                         </div>';
 
 
-		                $i += 1;
+						$i += 1;
 
 					endwhile;
 
@@ -100,10 +110,10 @@
 						echo '</div>';
 					}
 				} else {
-                    echo __( 'No products found' );
-                }
+					echo __( 'No products found' );
+				}
 				wp_reset_postdata();
-                ?>
+				?>
 
             </div>
         </div>
