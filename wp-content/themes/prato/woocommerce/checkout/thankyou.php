6fb1b041ef7b8bc02e5bf8a5dfc2f9f03a25accc
artemkopytko <kopytkoartem@gmail.com>
2018-05-03 18:48:16 +0300

    Thank you page styles

--- wp-content/themes/prato/woocommerce/checkout/thankyou.php
+++ wp-content/themes/prato/woocommerce/checkout/thankyou.php
@@ -19,9 +19,11 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 ?>
 
 <div class="woocommerce-order">
+<!--    <meta name="viewport" content="">-->
 
 	<?php if ( $order ) : ?>
 
@@ -38,45 +40,137 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<?php else : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); ?></p>
+            <main>
+                <section class="about">
+                    <div class="container about-content" style="flex-direction: column">
 
-			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+                        <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Спасибо! Ваш Заказ принят. Наш менеджер свяжется с Вами в ближайшее время', 'woocommerce' ), $order ); ?></p>
 
-				<li class="woocommerce-order-overview__order order">
-					<?php _e( 'Order number:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_order_number(); ?></strong>
-				</li>
+                        <style>
 
-				<li class="woocommerce-order-overview__date date">
-					<?php _e( 'Date:', 'woocommerce' ); ?>
-					<strong><?php echo wc_format_datetime( $order->get_date_created() ); ?></strong>
-				</li>
+                            .woocommerce-order-overview {
+                                border-radius: 5px;
+                            }
+                            .woocommerce-order-overview li {
+                                width: 75%;
+                                display: flex;
+                                flex-direction: column;
+                                padding: 10px;
+                                background-color: rgba(200,200,200,0.1);
+                                border-bottom: 1px solid #e7e7e7;
+                            }
+                            span {
+                                display: block;
+                                font-family: 'Open Sans', sans-serif;
+                                font-size: 13px;
+                                font-weight: 300;
+                                margin-bottom: 5px;
+                            }
+                            strong,.amount {
+                                display: block;
+                                font-family:'Helvetica Neue',sans-serif;
+                            }
+                            .amount {
+                                font-size: 16px;
+                            }
+                            .woocommerce-Price-currencySymbol {
+                                display: inline;
+                            }
+                            .wc-backward {
+                                display: block;
+                                color: #000;
+                                font-family: 'Open Sans', sans-serif;
+                                font-weight: 300;
+                                font-size: 16px;
+                                margin: 50px 0 100px 0;
+                                width: 220px;
+                                height: 50px;
+                                text-transform: uppercase;
+                                text-decoration: none;
+                                border: 1px solid #000;
+                                line-height: 50px;
+                                text-align: center;
+                                cursor: pointer;
+                                transition: all 0.4s ease-out;
+                                transition-delay: 0.2s;
+                            }
+                            .wc-backward:hover {
+                                transition-delay: 0.2s;
+                                color: #4CB69F;
+                                /*background-color: #4CB69F;*/
+                                border:1px solid #4CB69F;
+                                transition: all 0.4s ease-out;
+                            }
+                            .nb {
+                                border: none !important;
+                            }
+                            @media screen and (max-width: 450px) {
+                                .woocommerce-order-overview {
+                                    margin-top: 50px;
+                                }
+                                .container {
+                                    max-width: 90% !important;
+                                }
+                                .footer {
+                                    min-width: 1px !important;
+                                }
+                                .woocommerce-order-overview li {
+                                    width: 100%;
+                                }
+                            }
+                        </style>
+                        <ul style="padding: 0" class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
-				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
-					<li class="woocommerce-order-overview__email email">
-						<?php _e( 'Email:', 'woocommerce' ); ?>
-						<strong><?php echo $order->get_billing_email(); ?></strong>
-					</li>
-				<?php endif; ?>
+                            <li class="woocommerce-order-overview__order order">
+			                    <span><?php _e( 'Номер заказа:', 'woocommerce' ); ?></span>
+                                <strong><?php echo $order->get_order_number(); ?></strong>
+                            </li>
 
-				<li class="woocommerce-order-overview__total total">
-					<?php _e( 'Total:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_formatted_order_total(); ?></strong>
-				</li>
+                            <li class="woocommerce-order-overview__date date">
+			                    <span><?php _e( 'Дата:', 'woocommerce' ); ?></span>
+                                <strong><?php echo wc_format_datetime( $order->get_date_created() ); ?></strong>
+                            </li>
 
-				<?php if ( $order->get_payment_method_title() ) : ?>
-					<li class="woocommerce-order-overview__payment-method method">
-						<?php _e( 'Payment method:', 'woocommerce' ); ?>
-						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
-					</li>
-				<?php endif; ?>
+		                    <?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
+                                <li class="woocommerce-order-overview__email email">
+				                    <span><?php _e( 'Email:', 'woocommerce' ); ?></span>
+                                    <strong><?php echo $order->get_billing_email(); ?></strong>
+                                </li>
+		                    <?php endif; ?>
 
-			</ul>
+                            <li class="woocommerce-order-overview__total total">
+			                   <span> <?php _e( 'К Оплате:', 'woocommerce' ); ?></span>
+                                <strong><?php echo $order->get_formatted_order_total(); ?></strong>
+                            </li>
+
+		                    <?php if ( $order->get_payment_method_title() ) : ?>
+                                <li class="woocommerce-order-overview__payment-method method nb">
+				                    <span><?php _e( 'Способ оплаты:', 'woocommerce' ); ?></span>
+                                    <strong>
+					                    <?php
+					                    if($order->get_payment_method_title() == 'Cash on delivery')
+						                    echo 'Налычными при получении';
+					                    else
+						                    echo 'Банковский перевод'
+					                    ?>
+                                    </strong>
+                                </li>
+		                    <?php endif; ?>
+
+                        </ul>
+
+                        <a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', get_permalink( get_page_by_title( 'Домашняя Страница' ) ) ) )?>">
+		                    <?php _e( 'На главную', 'woocommerce' ) ?>
+                        </a>
+
+                    </div>
+                </section>
+            </main>
 
 		<?php endif; ?>
 
-		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
-		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
+<!--		--><?php //do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
+<!--		--><?php //do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
 
 	<?php else : ?>
 
